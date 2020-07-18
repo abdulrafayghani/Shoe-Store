5c390f88fa4db4ecf4123f5b3b4045787fb37849
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-07-18 17:44:06 +0500

    third commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import { AppBar, Toolbar, makeStyles, Button, Box } from '@material-ui/core';
+import { AppBar, Toolbar, Button, Box } from '@material-ui/core';
 import { useNavigate } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    // alignItems: 'center'
-  },
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   container: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     // alignItems: 'center'
+//   },
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// }));
 
 export const Header = () => {
-  const classes = useStyles();
-
   const navigate = useNavigate();
 
   return (
-    <div >
-      <AppBar  color="transparent" position='static'>
+    <div>
+      <AppBar color="transparent" position="static">
         <Toolbar>
-          <Box minWidth='100%' display='flex' justifyContent='center'>
-          <Box  >
-            <Button onClick={() => navigate('/')}>Home</Button>
-            <Button onClick={() => navigate('/products')}>Products</Button>
-          </Box>
+          <Box minWidth="100%" display="flex" justifyContent="center">
+            <Box>
+              <Button onClick={() => navigate('/')}>Home</Button>
+              <Button onClick={() => navigate('/products')}>Products</Button>
+            </Box>
           </Box>
         </Toolbar>
       </AppBar>
